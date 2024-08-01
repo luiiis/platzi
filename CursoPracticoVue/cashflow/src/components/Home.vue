c216b72acc878d5e920ae39525221a19cc7b66e2
luis <lgarciiaruiiz@gmail.com>
2024-08-01 11:57:39 -0600

    Ultimos detalles de la aplicacion con la grafica

--- CursoPracticoVue/cashflow/src/components/Home.vue
+++ CursoPracticoVue/cashflow/src/components/Home.vue
@@ -11,7 +11,7 @@
         :amount="amount"
       >
         <template #graphic>
-          <Graphic :amounts="amounts" />
+          <Graphic :amounts="amounts" @select="select" />
         </template>
         <template #action>
           <Action @create="create" />
@@ -63,7 +63,7 @@ export default {
         .map(m => m.amount);
       
       return lastDays.map((m, i) => {
-        const lastMovements = lastDays.slice(0, i);
+        const lastMovements = lastDays.slice(0, i + 1);
 
         return lastMovements.reduce((suma, movement) => {
           return suma + movement
@@ -98,6 +98,10 @@ export default {
     },
     save() {
       localStorage.setItem("movements", JSON.stringify(this.movements));
+    },
+    select(el) {
+      console.log(el);
+      this.amount = el;
     }
   }
 };
