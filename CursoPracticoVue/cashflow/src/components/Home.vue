b638866cd710c4a575b0394e7849ef5f6ae67cf9
luis <lgarciiaruiiz@gmail.com>
2024-07-31 20:43:21 -0600

    Modelo de datos con el grafico

--- CursoPracticoVue/cashflow/src/components/Home.vue
+++ CursoPracticoVue/cashflow/src/components/Home.vue
@@ -23,77 +23,121 @@
       </template>
     </Layout>
   </template>
-
-<script>
-import Layout from "./Layout.vue";
-import Header from "./Header.vue";
-import Resume from "./Resume/Index.vue";
-import Action from "./Action.vue";
-import Movements from "./Movements/Index.vue";
-import Graphic from "./Resume/Graphic.vue";
-
-export default {
+  
+  <script>
+  import Layout from "./Layout.vue";
+  import Header from "./Header.vue";
+  import Resume from "./Resume/Index.vue";
+  import Action from "./Action.vue";
+  import Movements from "./Movements/Index.vue";
+  import Graphic from "./Resume/Graphic.vue";
+  
+  export default {
     components: {
-    Layout,
-    Header,
-    Resume,
-    Action,
-    Movements,
-    Graphic,
-},
-data() {
-        return {
-            label: null,
-            amount: null,
-        }
-    },
+      Layout,
+      Header,
+      Resume,
+      Action,
+      Movements,
+      Graphic,
+  },
     data() {
-        return {
-            label: null,
-            amounts: [100, 200, 500, 200, -400, -600, -300, 0, 300, 500],
-            amount: null,
-            movements: [{
-                id: 0,
-                title: "Movimiento 1",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }, {
-                id: 1,
-                title: "Movimiento 2",
-                description: "Lorem ipsum dolor sit amet",
-                amount: -1000,
-            }, {
-                id: 2,
-                title: "Movimiento 3",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }, {
-                id: 3,
-                title: "Movimiento 4",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }, {
-                id: 4,
-                title: "Movimiento 5",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }, {
-                id: 6,
-                title: "Movimiento 7",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }, {
-                id: 7,
-                title: "Movimiento 8",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }, {
-                id: 8,
-                title: "Movimiento 9",
-                description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
-            }]
-        }
+      return {
+        label: null,
+        amount: null,
+        movements: [
+          {
+            id: 0,
+            title: "Movimiento 1",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 100,
+            time: new Date("07-13-2024"),
+          },
+          {
+            id: 1,
+            title: "Movimiento 2",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 200,
+            time: new Date("07-12-2024"),
+          },
+          {
+            id: 2,
+            title: "Movimiento 3",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 500,
+            time: new Date("07-11-2024"),
+          },
+          {
+            id: 3,
+            title: "Movimiento 4",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 200,
+            time: new Date("07-10-2024"),
+          },
+          {
+            id: 4,
+            title: "Movimiento 5",
+            description: "Lorem ipsum dolor sit amet",
+            amount: -400,
+            time: new Date("07-29-2024"),
+          },
+          {
+            id: 5,
+            title: "Movimiento 6",
+            description: "Lorem ipsum dolor sit amet",
+            amount: -600,
+            time: new Date("07-04-2024"),
+          },
+          {
+            id: 6,
+            title: "Movimiento 7",
+            description: "Lorem ipsum dolor sit amet",
+            amount: -300,
+            time: new Date("07-07-2024"),
+          },
+          {
+            id: 7,
+            title: "Movimiento 8",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 100,
+            time: new Date("07-26-2024"),
+          },
+          {
+            id: 8,
+            title: "Movimiento 9",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 300,
+            time: new Date("07-22-2024"),
+          },
+          {
+            id: 9,
+            title: "Movimiento 10",
+            description: "Lorem ipsum dolor sit amet",
+            amount: 500,
+            time: new Date("07-11-2024"),
+          },
+        ],
+      };
     },
-};
-</script>
+    computed: {
+      amounts() {
+        const lastDays = this.movements
+          .filter(m => {
+            const today = new Date();
+            const oldDate = today.setDate(today.getDate() - 30);
+  
+            return m.time > oldDate;
+          })
+          .map(m => m.amount);
+        
+        return lastDays.map((m, i) => {
+          const lastMovements = lastDays.slice(0, i);
+  
+          return lastMovements.reduce((suma, movement) => {
+            return suma + movement
+          }, 0);
+        });
+      }
+    },
+  };
+  </script>
