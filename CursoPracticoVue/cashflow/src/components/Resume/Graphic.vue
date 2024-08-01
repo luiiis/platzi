532405134a5d40f5d3e823c8b7b7c7e36570466a
luis <lgarciiaruiiz@gmail.com>
2024-07-31 20:20:18 -0600

    Creando la interaccion con el grafico

--- CursoPracticoVue/cashflow/src/components/Resume/Graphic.vue
+++ CursoPracticoVue/cashflow/src/components/Resume/Graphic.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <svg
+            @touchstart="tap"
+            @touchmove="tap"
+            @touchend="untap"
             viewBox="0 0 300 200"
         >
             <line
@@ -15,14 +18,15 @@
                 fill="none"
                 stroke="#0689B0"
                 stroke-width="2"
-                 :points="points"
+                :points="points"
             />
             <line
+                v-show="showPointer"
                 stroke="#04b500"
                 stroke-width="2"
-                x1="200"
+                :x1="pointer"
                 y1="0"
-                x2="200"
+                :x2="pointer"
                 y2="200"
             />
         </svg>
@@ -31,9 +35,8 @@
     </div>
 </template>
 
-
 <script setup>
-import { toRefs, defineProps, computed } from 'vue';
+import { ref, toRefs, defineProps, computed } from 'vue';
 
 const props = defineProps({
     amounts: {
@@ -63,10 +66,24 @@ const points = computed(() => {
     return amounts.value.reduce((points, amount, i) => {
         const x = (300 / total) * (i + 1);
         const y = amountToPixels(amount);
-        console.log(y);
         return `${points} ${x},${y}`;
     }, "0, 100");
 });
+
+const showPointer = ref(false);
+const pointer = ref(0);
+
+const tap = ({ target, touches }) => {
+    showPointer.value = true;
+    const elementWidth = target.getBoundingClientRect().width;
+    const elementX = target.getBoundingClientRect().x;
+    const touchX = touches[0].clientX;
+    pointer.value = ((touchX - elementX) * 300) / elementWidth;
+}
+
+const untap = () => {
+    showPointer.value = false;
+}
 </script>
 
 <style scoped>
