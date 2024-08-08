b0df1077d6a9e04d3d6e15bbce13b864aa01cb9b
luis <lgarciiaruiiz@gmail.com>
2024-08-07 21:16:04 -0600

    paginacion de consultas

---
+++ CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/persistence/repository/PizzaPagSortRepository.java
@@ -0,0 +1,7 @@
+package com.pizza.persistence.repository;
+
+import com.pizza.persistence.entity.PizzaEntity;
+import org.springframework.data.repository.ListPagingAndSortingRepository;
+
+public interface PizzaPagSortRepository extends ListPagingAndSortingRepository<PizzaEntity, Integer> {
+}
