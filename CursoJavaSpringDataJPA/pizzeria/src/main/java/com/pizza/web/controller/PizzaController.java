6643426f8d2b570484f048a9943ca2f9df6b9b3e
luis <lgarciiaruiiz@gmail.com>
2024-08-08 16:30:43 -0600

    Usar @Modifying en @Query

--- CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/web/controller/PizzaController.java
+++ CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/web/controller/PizzaController.java
@@ -1,7 +1,9 @@
 package com.pizza.web.controller;
 
+import com.pizza.persistence.entity.OrderEntity;
 import com.pizza.persistence.entity.PizzaEntity;
 import com.pizza.service.PizzaService;
+import com.pizza.service.dto.UpdatePizzaPriceDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -78,6 +80,16 @@ public class PizzaController {
     public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza) {
         if (pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza())) {
             return ResponseEntity.ok(this.pizzaService.save(pizza));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping("/price")
+    public ResponseEntity<Void> updatePrice(@RequestBody UpdatePizzaPriceDto dto) {
+        if (this.pizzaService.exists(dto.getPizzaId())) {
+            this.pizzaService.updatePrice(dto);
+            return ResponseEntity.ok().build();
         }
 
         return ResponseEntity.badRequest().build();
