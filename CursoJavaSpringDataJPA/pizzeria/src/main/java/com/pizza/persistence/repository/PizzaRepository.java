0b0f7d76cfa5c0671338586c845231b30c9d5785
luis <lgarciiaruiiz@gmail.com>
2024-08-07 20:18:15 -0600

    Query Methods:And, or,True,IgnoreCase, OrderBy

--- CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/persistence/repository/PizzaRepository.java
+++ CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/persistence/repository/PizzaRepository.java
@@ -3,5 +3,8 @@ package com.pizza.persistence.repository;
 import com.pizza.persistence.entity.PizzaEntity;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
 public interface PizzaRepository extends ListCrudRepository<PizzaEntity, Integer> {
+    List<PizzaEntity> findAllByAvailableTrueOrderByPrice();
+    PizzaEntity findAllByAvailableTrueAndNameIgnoreCase(String name);
 }
--- CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/web/controller/PizzaController.java
+++ CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/web/controller/PizzaController.java
@@ -35,6 +35,17 @@ public class PizzaController {
         return ResponseEntity.ok(this.pizzaService.get(idPizza));
     }
 
+
+    @GetMapping("/available")
+    public ResponseEntity<List<PizzaEntity>> getAvailable() {
+        return ResponseEntity.ok(this.pizzaService.getAvailable());
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<PizzaEntity> getByName(@PathVariable String name) {
+        return ResponseEntity.ok(this.pizzaService.getByName(name));
+    }
+
     @PostMapping
     public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza) {
         if (pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza())) {
