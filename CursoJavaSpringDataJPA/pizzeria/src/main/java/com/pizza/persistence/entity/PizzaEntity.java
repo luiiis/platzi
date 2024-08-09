1e02a00e06c82e331009828840f1fcedc902c03f
luis <lgarciiaruiiz@gmail.com>
2024-08-08 18:18:58 -0600

    Auditoria con Spring Data

--- CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/persistence/entity/PizzaEntity.java
+++ CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/persistence/entity/PizzaEntity.java
@@ -1,21 +1,30 @@
 package com.pizza.persistence.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.Modifying;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "pizza")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
-public class PizzaEntity {
+public class PizzaEntity extends AuditableEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_pizza", nullable = false)
@@ -38,5 +47,6 @@ public class PizzaEntity {
 
     @Column(columnDefinition = "TINYINT", nullable = false)
     private Boolean available;
+
 
 }
