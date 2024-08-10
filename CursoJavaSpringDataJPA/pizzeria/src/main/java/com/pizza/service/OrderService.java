c3b6399acde5ebf7562ccf1ed8d98c917e0af066
luis <lgarciiaruiiz@gmail.com>
2024-08-09 18:41:45 -0600

    Ejecutar un sotored Procedure

--- CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/service/OrderService.java
+++ CursoJavaSpringDataJPA/pizzeria/src/main/java/com/pizza/service/OrderService.java
@@ -3,8 +3,10 @@ package com.pizza.service;
 import com.pizza.persistence.entity.OrderEntity;
 import com.pizza.persistence.projection.OrderSummary;
 import com.pizza.persistence.repository.OrderRepository;
+import com.pizza.service.dto.RandomOrderDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -44,5 +46,10 @@ public class OrderService {
 
     public OrderSummary getSummary(int orderId) {
         return this.orderRepository.findSummary(orderId);
+    }
+
+    @Transactional
+    public boolean saveRandomOrder(RandomOrderDto randomOrderDto) {
+        return this.orderRepository.saveRandomOrder(randomOrderDto.getIdCustomer(), randomOrderDto.getMethod());
     }
 }
